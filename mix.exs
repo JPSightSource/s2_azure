efc42f62345113ad3440df46222e189bbe09da44
James Parker <jp@sightsource.net>
2025-01-21 12:04:32 -0500

    Made changes to Dep

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule S2Azure.Mixfile do
 
   defp deps do
     [
-      {:erlazure, git: "https://github.com/JPSightSource/s2_erlazure.git", manager: :rebar},
+      {:erlazure, git: "https://github.com/JPSightSource/s2_erlazure.git", manager: :rebar3},
       {:ex_doc  , "~> 0.11.5", only: [:dev, :test]},
       {:earmark , "~> 0.2.1" , only: [:dev, :test]},
     ]
