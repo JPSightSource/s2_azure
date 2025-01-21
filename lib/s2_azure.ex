d38ac7b5c530c7fee923c4716b499184a194889c
James Parker <jp@sightsource.net>
2025-01-21 11:41:07 -0500

    Updated Elixir Syntax

--- lib/s2_azure.ex
+++ lib/s2_azure.ex
@@ -9,8 +9,8 @@ defmodule S2Azure do
 
   def defaults(config) do
     config
-    |> Dict.put_new(:account   , Application.get_env(:s2_azure, :account   ))
-    |> Dict.put_new(:access_key, Application.get_env(:s2_azure, :access_key))
+    |> Keyword.put_new(:account   , Application.get_env(:s2_azure, :account   ))
+    |> Keyword.put_new(:access_key, Application.get_env(:s2_azure, :access_key))
   end
 
   def config do
@@ -42,14 +42,13 @@ defmodule S2Azure do
   defp do_request(action, args, opts) when is_atom(action) do
     args = args |> Utils.normalize_to_charlist
 
-    client = opts |> Dict.get(:client, client())
+    client = opts |> Keyword.get(:client, client())
     apply(:erlazure, action, [client] ++ args)
     |> parse_response
   end
 
   defp parse_response({:error, _} = response), do: response
   defp parse_response({:ok   , _} = response), do: response
-
   defp parse_response({body, headers}) do
     %{ body: body, headers: headers }
   end
